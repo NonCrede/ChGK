43f5890057376d4ddfcd9302e2f71938e1df685d
NonCrede <nikitaomsk03@gmail.com>
2021-05-13 02:06:37 +0600

    kekw

--- mine.js
+++ mine.js
@@ -1,11 +1,13 @@
 let ptr = document.querySelector(".wheel_hand");
-console.log(ptr);
 let animation = ptr.animate([
-    {transform: "rotate("+0 + "deg)"},
-    {transform: "rotate("+430 + "deg)"}
-], 1000);
+    {transform: "rotate("+0 + "deg)" },
+    {transform: "rotate("+430 + "deg)" },
+], {
+    easing: "ease-in-out",
+    duration: 1000,
+});
 animation.addEventListener("finish", function(){
     ptr.style.transform = "rotate("+430 + "deg)";
 });
 //ptr.style.transform = "rotate("+45 + "deg)";
-ptr.style.transition = "transform "+1000 +"ms";
+//ptr.style.transition = "transform "+1000 +"ms";
