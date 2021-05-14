52e0ab0d669b1d7177437c57e68621d421a91829
NonCrede <nikitaomsk03@gmail.com>
2021-05-14 15:48:58 +0600

    kekw

--- mine.js
+++ mine.js
@@ -1,13 +1,42 @@
+let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,16,18,19,20];
+let array2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+for (let i = 0; i < 100; i++) {
+
+    let rand_number = Math.floor(Math.random() * array.length);
+    let temp = array[rand_number];
+
+    let rand = Math.floor(Math.random() * array.length);
+    array[rand_number] = array[rand];
+    array[rand] = temp;
+}
+let offset = 0;
+let sector = 360 / array.length;
+
+for (let i = 0; i < array.length; i++) {
+    let r = 360 * (Math.floor(Math.random() * 3) + 1);
+    let q = 0;
+    if (i != 0) {
+        q = array[i - 1];
+    }
+    if (q < array[i]) {
+        array2[i] = offset + (array[i] - q) * sector + r;
+    }else {
+        array2[i] = offset + (360 - (q - array[i]) * sector) + r;
+    }
+    if (i == 0) {
+        array2[i] =  array2[i] - sector / 2;
+    }
+    offset =  array2[i];
+}
+console.log(array);
+console.log(array2);
 let ptr = document.querySelector(".wheel_hand");
-let animation = ptr.animate([
-    {transform: "rotate("+0 + "deg)" },
-    {transform: "rotate("+430 + "deg)" },
-], {
-    easing: "ease-in-out",
-    duration: 1000,
-});
-animation.addEventListener("finish", function(){
-    ptr.style.transform = "rotate("+430 + "deg)";
+let o = 0;
+ptr.addEventListener("click", function(){
+    ptr.style.transition = "transform "+2500 +"ms";
+    ptr.style.transform = "rotate("+ array2[o]+ "deg)";
+    o = o + 1;
 });
 //ptr.style.transform = "rotate("+45 + "deg)";
 //ptr.style.transition = "transform "+1000 +"ms";
